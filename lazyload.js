de1617c2ffef83f4283adfa775c0899bdb330a89
guxianwang <guxianwang@2144.cn>
2017-09-30 12:14:15 +0800

    lazy bug

--- lazyload.js
+++ lazyload.js
@@ -3,35 +3,31 @@
  *    lazyload({
  *        elem : [Y.$id(xx).getElementsByTagName('img'),Y.$id(xx2).getElementsByTagName('img')],
  *        original : 'data-original',
- *        container : 'document',
- *        event : 'scroll',
- *        fadeIn : true
+ *
  *    });
  */
 lazyload = function( config ){
    // var config = {
    //      ele        :   obj.ele,
    //      original    :  obj.original || 'data-src', // String          存放图片真实地址的属性
-   //      container   :   ( config.container ? config.container : (document.body || document.documentElement) ),        // DOM / Selector 默认的容器为document，可自定义容器
-   //      event       :   'scroll',   // String            触发事件类型，默认为window.onscroll事件
    //      animate      :   animate,      // Boolean        是否使用fadeIn效果来显示
    //  };
 
-   var ele = config.ele,//lazy加载父级容器
+   var doc = document,
+       doc_body = doc.body || doc.documentElement,
+       ele = config.ele || doc,//lazy加载父级容器
        imgAry = [], //lazy元素数组
        original = config.original || 'data-src',
-       doc = document,
-       doc_body = doc.body || doc.documentElement,
        lazyNum = 0, //已加载的数量
        heightAry = [],//图片高度的数组集合
        distance = config.distance || 200,//lazy加载距离
        animate = config.animate;
 
    function initElementMap() { //遍历获取图片集合
-       var imgs = ele.querySelectorAll(ele + '[' + original +']'),
+       var imgs = ele.querySelectorAll('img[' + original +']'),
            i, j, len, lylen;
        //获取lazy图数组
-       for(i = 0; len = imgs.length; i < len; i++){
+       for(i = 0, len = imgs.length; i < len; i++){
            imgAry.push( imgs[i] );
        }
 
@@ -50,7 +46,7 @@ lazyload = function( config ){
             imgShowHeight = distance + downScrollTop,//图片显示的高度
             l,len;
 
-        for( l = 0; len = imgAry.length; l < len; l++){
+        for( l = 0, len = imgAry.length; l < len; l++){
             if( imgShowHeight > heightAry[l]){
                 imgAry[l].src = imgAry[l].getAttribute(original);
                 imgAry[l].removeAttribute(original);
@@ -66,7 +62,7 @@ lazyload = function( config ){
         }
         var offsetTop = element.offsetTop,
             parent = element.offsetParent;
-        while( parnet !== null ){
+        while( parent !== null ){
             offsetTop += parent.offsetTop;
             parent = parent.offsetParent;
         }
@@ -80,6 +76,6 @@ lazyload = function( config ){
     }
 
     //绑定事件,依赖base
-    query.add(doc.body,'scroll', init);
+    query.add(window,'scroll', init);
 
 };
