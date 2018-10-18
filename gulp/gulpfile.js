71190ae576821630dd93782d77cd53b4145576d8
gintangible <gintangible@163.com>
2018-10-18 11:07:12 +0800

    add image sprite

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
 const runSequence = require('run-sequence'); //run-sequence  逐步执行任务
 const del = require('del');
+const fs = require('fs');
+const path = require('path');
 const moment = require('moment');
 const plugins = require('gulp-load-plugins')(); //for gulp
+const spritesmith = require('gulp.spritesmith');
 /* plugins :{
  *   sass:                 ： 
  *   gulp-imagemin         ：  压缩img
@@ -16,7 +19,7 @@ const plugins = require('gulp-load-plugins')(); //for gulp
  * }
  */
 var env = process.env.GULP_ENV && process.env.GULP_ENV.replace(/(\s*$)|(^\s*)/ig, '');
-var prod = env === 'prod';
+var prod = true;
 const src = 'src/';
 const dist = 'dist/';
 const paths = {
@@ -39,6 +42,11 @@ const paths = {
     js: {
         src: src + 'js/*.js',
         dist: dist + 'js/'
+    },
+    minImg: {
+        src: src + 'images/',
+        outSrc: 'images/',
+        outCss: 'css/'
     }
 };
 
@@ -57,7 +65,46 @@ for (let pro in taskSrc) {
     })
 }
 
+// 图片雪碧图 + css
+function getFolders(dir) { //获取文件目录
+    return fs.readdirSync(dir)
+        .filter(function (file) {
+            return fs.statSync(path.join(dir, file)).isDirectory();
+        });
+}
 
+
+const imageSpirt = getFolders(src + paths.minImg.src);
+imageSpirt.length && imageSpirt.forEach(
+    icon => {
+        gulp.task(icon, function () {
+            gulp.src(paths.minImg.src + icon + '/*.png')
+                .pipe(spritesmith({
+                    imgName: paths.minImg.outSrc + icon + '.png', //保存合并后的名称
+                    cssName: paths.minImg.outCss + icon + '.css', //保存合并后css样式的地址
+                    padding: 10, //合并时两个图片的间距
+                    algorithm: 'binary-tree', //注释1
+                    //cssTemplate:'css/handlebarsStr.css'    //注释2
+                    cssTemplate: function (data) { //如果是函数的话，这可以这样写
+                        var arr = [];
+                        data.sprites.forEach(function (sprite) {
+                            arr.push("." + icon + "-" + sprite.name +
+                                "{" +
+                                "background-image: url('" + sprite.escaped_image + "');" +
+                                "background-repeat: no-repeat;" +
+                                "background-position: " + sprite.px.offset_x + " " + sprite.px.offset_y + ";" +
+                                "background-size: " + sprite.px.width + " " + sprite.px.height + ";" +
+                                "width: " + sprite.px.width + ";" +
+                                "height: " + sprite.px.height + ";" +
+                                "}\n");
+                        });
+                        return arr.join("");
+                    }
+                }))
+                .pipe(gulp.dest(dist)); //输出目录
+        });
+    }
+);
 
 gulp.task('html', () => {
     return gulp.src(paths.html.src)
@@ -68,16 +115,16 @@ gulp.task('html', () => {
 
 // 编译Sass
 gulp.task('sass', () => {
-    return gulp.src(paths.sass.src)      
+    return gulp.src(paths.sass.src)
         .pipe(plugins.sass().on('error', plugins.sass.logError)) //watch 不会中断
         .pipe(plugins.autoprefixer({
-            browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
+            browsers: ['last 4 version']
         }))
         .pipe(plugins.if(prod, plugins.cleanCss({
             keepBreaks: false,
             compatibility: 'ie8',
             keepSpecialComments: '*'
-        })))   
+        })))
         .pipe(gulp.dest(paths.sass.dist))
         .pipe(plugins.connect.reload());
 });
@@ -100,21 +147,21 @@ gulp.task('temp', () => {
 
 
 gulp.task('js', () => {
-    return gulp.src(paths.js.src+ '*.js')
+    return gulp.src(paths.js.src)
         .pipe(plugins.jshint())
-        .pipe(plugins.if(prod, plugins.uglify()))
+        // .pipe(plugins.if(prod, plugins.uglify()))
         .pipe(gulp.dest(paths.js.dist))
         .pipe(plugins.connect.reload());
 });
 
 // zip
-gulp.task('zip', function() {
+gulp.task('zip', function () {
     del('*.zip');
     const dir = __dirname.split('\\');
     const name = dir[dir.length - 1];
     const time = moment(new Date).format('YYYY-MM-DD');
-
     return gulp.src(dist + '**')
+        // return gulp.src('**')
         .pipe(plugins.zip(name + time + '.zip'))
         .pipe(gulp.dest('./'));
 });
@@ -128,18 +175,35 @@ gulp.task('server', () => {
     });
 });
 
-gulp.task('watch', ['server'], function() {
+gulp.task('watch', ['server'], function () {
     paths.html.src && gulp.watch(paths.html.src, ['html']);
     paths.sass.src && gulp.watch(paths.sass.src, ['sass']);
     paths.images.src && gulp.watch(paths.images.src, ['image']);
     paths.temp.src && gulp.watch(paths.temp.src, ['temp']);
-    paths.js.src && gulp.watch(paths.js.src, ['js']);
-    
-    // paths.js.src && gulp.watch(paths.js.src, ['js', ...Object.keys(taskSrc)]);
+
+    if (taskSrc.length) {
+        paths.js.src && gulp.watch(paths.js.src, [...Object.keys(taskSrc)]);
+    } else {
+        paths.js.src && gulp.watch(paths.js.src, ['js']);
+    }
+
+    if (imageSpirt.length) {
+        imageSpirt.forEach(img => {
+            paths.minImg.src + img && gulp.watch(paths.minImg.src + img, [...imageSpirt]);
+        })
+    }
+
 });
 
 gulp.task('default', () => {
-    runSequence(['sass', 'html', 'image', 'temp', 'js'], 'watch');
+
+    if (taskSrc.length) {
+        paths.js.src && gulp.watch(paths.js.src, [...Object.keys(taskSrc)]);
+    } else {
+        runSequence(['sass', 'html', 'image', 'temp', 'js'], 'watch');
+    }
+
+    // runSequence(['sass', 'html', 'image', 'temp', 'js', ...imageSpirt], 'watch');
 });
 
 gulp.task('clean', callback => {
@@ -147,7 +211,7 @@ gulp.task('clean', callback => {
     del('dist').then(paths => callback());
 });
 
-gulp.task('build', ['clean'], function(callback) {
+gulp.task('build', ['clean'], function (callback) {
     prod = true;
     runSequence(['sass', 'html', 'image', 'temp', 'js']);
 });
